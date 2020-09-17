20803558fe3b59ad2da0784983b2a3ad4363694c
marcos <mfonsecabarros@gmail.com>
2020-09-17 11:57:29 -0300

    gulp Functions

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,31 @@
+const { watch } = require('gulp');
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+var imagemin = require('gulp-imagemin');
+var svgmin = require('gulp-svgmin');
 
 
-gulp.task('default', function() {
-    console.log('Gulp funcionando...');
-  });
+function imgMin() {
+  return gulp
+    .src('src/static/imagens/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest("src/static/minified/img"))
+}
+
+function svgMin() {
+  return gulp
+    .src('src/static/icones/*')
+    .pipe(svgmin())
+    .pipe(gulp.dest("src/static/minified/svg"))
+}
+
+
+
+gulp.task('svgMin', svgMin);
+gulp.task('imgMin', imgMin);
+
+gulp.task('watch', () => {
+  gulp.watch('src/static/imagens/*', imgMin);
+  gulp.watch('src/static/icones/*', svgMin);
+});
+
+gulp.task("default", gulp.series("imgMin", "svgMin", "watch"));
